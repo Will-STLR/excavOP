3b63e98845a3650704f5c9998e176440fd418a51
kiwi2 <kiwi2@excav.de>
2025-05-07 13:12:07 +0200

    commit

--- app/src/main/java/com/example/excavop/ui/theme/MeshVao.kt
+++ app/src/main/java/com/example/excavop/ui/theme/MeshVao.kt
@@ -8,10 +8,53 @@ import java.nio.ByteOrder
 class MeshVao: GlVao {
     private var vao: Int = 0
     private val vertices = floatArrayOf(
-        //  x,     y,     r,    g,    b
-        0.0f,  0.5f,  1.0f, 0.0f, 0.0f, 1.0f,  // oben (rot)
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f,  // links unten (grün)
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f // rechts unten (blau)
+        // Vorderseite (z = 0)
+        0f, 0f, 0f,    1f, 0f, 0f, 1f, // P0
+        1f, 0f, 0f,    1f, 0f, 0f, 1f, // P1
+        1f, 1f, 0f,    1f, 0f, 0f, 1f, // P2
+        0f, 0f, 0f,    1f, 0f, 0f, 1f, // P0
+        1f, 1f, 0f,    1f, 0f, 0f, 1f, // P2
+        0f, 1f, 0f,    1f, 0f, 0f, 1f, // P3
+
+        // Rückseite (z = 1)
+        0f, 0f, 1f,    0f, 1f, 0f, 1f, // P4
+        1f, 1f, 1f,    0f, 1f, 0f, 1f, // P6
+        1f, 0f, 1f,    0f, 1f, 0f, 1f, // P5
+        0f, 0f, 1f,    0f, 1f, 0f, 1f, // P4
+        0f, 1f, 1f,    0f, 1f, 0f, 1f, // P7
+        1f, 1f, 1f,    0f, 1f, 0f, 1f, // P6
+
+        // Linke Seite (x = 0)
+        0f, 0f, 0f,    1f, 1f, 1f, 1f,
+        0f, 1f, 0f,    1f, 1f, 1f, 1f,
+        0f, 1f, 1f,    1f, 1f, 1f, 1f,
+        0f, 0f, 0f,    1f, 1f, 1f, 1f,
+        0f, 1f, 1f,    1f, 1f, 1f, 1f,
+        0f, 0f, 1f,    1f, 1f, 1f, 1f,
+
+        // Rechte Seite (x = 1)
+        1f, 0f, 0f,    1f, 1f, 0f, 1f,
+        1f, 1f, 0f,    1f, 1f, 0f, 1f,
+        1f, 1f, 1f,    1f, 1f, 0f, 1f,
+        1f, 0f, 0f,    1f, 1f, 0f, 1f,
+        1f, 1f, 1f,    1f, 1f, 0f, 1f,
+        1f, 0f, 1f,    1f, 1f, 0f, 1f,
+
+        // Oben (y = 1)
+        0f, 1f, 0f,    1f, 0f, 1f, 1f,
+        1f, 1f, 0f,    1f, 0f, 1f, 1f,
+        1f, 1f, 1f,    1f, 0f, 1f, 1f,
+        0f, 1f, 0f,    1f, 0f, 1f, 1f,
+        1f, 1f, 1f,    1f, 0f, 1f, 1f,
+        0f, 1f, 1f,    1f, 0f, 1f, 1f,
+
+        // Unten (y = 0)
+        0f, 0f, 0f,    0f, 1f, 1f, 1f,
+        1f, 0f, 1f,    0f, 1f, 1f, 1f,
+        1f, 0f, 0f,    0f, 1f, 1f, 1f,
+        0f, 0f, 0f,    0f, 1f, 1f, 1f,
+        0f, 0f, 1f,    0f, 1f, 1f, 1f,
+        1f, 0f, 1f,    0f, 1f, 1f, 1f
     )
 
     override fun buildVao() {
@@ -26,14 +69,14 @@ class MeshVao: GlVao {
 
         GLES32.glBindBuffer(GLES32.GL_ARRAY_BUFFER, vbo)
         //first para: vertices, second: dim(3 for each vertex and 4 for color), third: sizeof(float)
-        val tempBuffer = ByteBuffer.allocateDirect(3 * (2+4) * 4).order(ByteOrder.nativeOrder()).asFloatBuffer()
+        val tempBuffer = ByteBuffer.allocateDirect(36 * (3+4) * 4).order(ByteOrder.nativeOrder()).asFloatBuffer()
         tempBuffer.put(vertices).position(0)
 
         //we need for each layout an attribpointer
-        GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER, 3 * (2+4) * 4, tempBuffer, GLES32.GL_STATIC_DRAW)
-        GLES32.glVertexAttribPointer(0, 2, GLES32.GL_FLOAT, false, 6*4, 0)
+        GLES32.glBufferData(GLES32.GL_ARRAY_BUFFER, 36 * (3+4) * 4, tempBuffer, GLES32.GL_STATIC_DRAW)
+        GLES32.glVertexAttribPointer(0, 3, GLES32.GL_FLOAT, false, 7*4, 0)
         GLES32.glEnableVertexAttribArray(0)
-        GLES32.glVertexAttribPointer(1, 4, GLES32.GL_FLOAT, false, 6*4, 8)
+        GLES32.glVertexAttribPointer(1, 4, GLES32.GL_FLOAT, false, 7*4, 12)
         GLES32.glEnableVertexAttribArray(1)
 
         GLES32.glBindVertexArray(0) //Finished with this VAO
@@ -41,7 +84,7 @@ class MeshVao: GlVao {
 
     override fun render() {
         GLES32.glBindVertexArray(vao)
-        GLES32.glDrawArrays(GLES32.GL_TRIANGLES, 0, 3)
+        GLES32.glDrawArrays(GLES32.GL_TRIANGLES, 0, 36)
         GLES32.glBindVertexArray(0)
     }
 }
@@ -50,7 +93,7 @@ class MeshProg: GLProgram(vertexShaderCode, fragmentShaderCode) {
         private const val TAG = "MeshProg"
         private val vertexShaderCode = """
           #version 320 es
-          layout(location = 0) in vec2 vPosition;
+          layout(location = 0) in vec3 vPosition;
           layout(location = 1) in vec4 vColor;
            
           uniform mat4 uView;
@@ -60,9 +103,9 @@ class MeshProg: GLProgram(vertexShaderCode, fragmentShaderCode) {
           out vec4 color;
             
           void main() { 
-            vec2 pos = vPosition;
-            pos.y += sin(pos.x * 10.0 + uTime) * 0.1;
-            gl_Position = uProjection * uView * vec4(pos, 0.0f, 1.0f);
+            vec3 pos = vPosition;
+            pos.z += sin(pos.x * 10.0 + uTime) * 0.1;
+            gl_Position = uProjection * uView * vec4(pos, 1.0f);
             
             float wave = sin(pos.x * 10.0 + uTime);
             float intensity = (wave + 1.0) * 0.5;
